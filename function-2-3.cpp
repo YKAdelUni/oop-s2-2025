1cb6e1ae8cb98235619c277dbd7adee570497429
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2025-08-08 12:15:28 +0930

    function 2-3

---
+++ function-2-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool is_palindrome(int integers[], int length){
+    if (length < 1){
+        return false;
+    }
+    for (int i = 0; i < length / 2; i++){
+        if (integers[i] != integers[length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int sum_array_elements (int integers[], int length){
+    int sum = 0;
+    for (int i = 0; i < length; i++){
+        sum += integers[i];
+    }
+    return sum;
+}
+
+int sum_if_palindrome (int integers[], int length){
+    if (is_palindrome(integers,length)){
+        return sum_array_elements(integers,length);
+    } else {return -2;}
+}
