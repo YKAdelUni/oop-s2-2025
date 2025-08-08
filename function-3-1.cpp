12ebd736336c74c65637ec679408fbc335ec39e6
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2025-08-08 14:23:49 +0930

    -

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -4,8 +4,8 @@ bool is_fanarray(int array[], int n) {
     if (n < 1){
         return false;
     }
-    for (int i = 0; i < n / 2; i++){
-        if (array[i] != array[n - 1 - i]){
+    for (int i = 0; i < n/2; i++){
+        if (array[i] != array[n-1-i]){
             return false;
         }
     }
