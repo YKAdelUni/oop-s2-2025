a66ed96c6e21b7ece95b5f2f7ff10932e86c4fb4
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2025-09-10 10:20:09 +0930

    Removed files

--- Bus.cpp
+++
@@ -1,9 +0,0 @@
-#include "Bus.h"
-
-Bus::Bus(int ID) : Vehicle(ID) {}
-
-int Bus::getParkingDuration(){
-    std::time_t presentTime = std::time(nullptr);
-    int parkingDuration = static_cast<int>(std::difftime(presentTime, timeOfEntry));
-    return parkingDuration*0.75;
-}
--- Vehicle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vehicle.h"
-
-Vehicle::Vehicle(int ID) {
-    timeOfEntry = std::time(nullptr);
-    this->ID = ID;
-}
-
-int Vehicle::getID() {return ID;}
