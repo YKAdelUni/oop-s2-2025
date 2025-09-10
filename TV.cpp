d610fd79b5e1d614452d62d6a560bab887049685
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2025-09-11 03:44:29 +0930

    Submision

---
+++ TV.cpp
@@ -0,0 +1,19 @@
+#include "TV.h"
+
+TV::TV(){
+    powerRating = 0;
+    screenSize = 0;
+    isOn = false;
+}
+
+TV::TV(int powerRating, double screenSize){
+    this->powerRating = powerRating;
+    this->screenSize = screenSize;
+    isOn = false;
+}
+
+void TV::setScreenSize(double screenSize){this->screenSize = screenSize;}
+
+double TV::getScreenSize(){return screenSize;}
+
+double TV::getPowerConsumption(){return powerRating*(screenSize/10);}
